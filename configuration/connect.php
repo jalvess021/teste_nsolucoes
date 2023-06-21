d077a84657e985acebfb8910813a3b2116e16e9c
jalvess021 <jalvess021@gmail.com>
2023-06-21 15:27:19 -0300

    configurando arquivos  docker

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,5 +1,5 @@
 <?php
      date_default_timezone_set ("America/Sao_Paulo");
-    $connect = mysqli_connect('localhost', 'root', '', 'teste_nsolucoes');
+    $connect = mysqli_connect('db', 'root', 'root', 'teste_nsolucoes');
     mysqli_set_charset($connect, "utf8");
 ?>
